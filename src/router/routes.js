482d415c1ef22002b5971cd8059e663250fa26e0
Sameera Alam <alamsameera26@gmail.com>
2022-09-23 02:45:04 +0500

    Updated

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,7 @@ const routes = [
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "", component: () => import("pages/Landing.vue") },
       {
         path: "/home",
         component: (homeBranding) => import("../components/home.vue"),
